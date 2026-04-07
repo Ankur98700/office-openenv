fb9b793ad050521ec8c21c27b10a849a13fd8f54
Ankur98700 <231642312+Ankur98700@users.noreply.github.com>
2026-04-07 23:12:15 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,24 +1,13 @@
-import os
-from openai import OpenAI
 from env import OfficeEnv
 
-client = OpenAI(api_key=os.getenv("HF_TOKEN"))
-
 env = OfficeEnv()
+
 obs = env.reset()
+print("Task:", obs)
 
-print("Task:", obs["task"]["question"])
+# Dummy action (for testing)
+action = "spam"
 
-response = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "user", "content": obs["task"]["question"]}
-    ]
-)
+obs, reward, done, info = env.step(action)
 
-answer = response.choices[0].message.content
-print("Model Answer:", answer)
-
-obs, reward, done, _ = env.step(answer)
-
-print("Score:", reward)
+print("Reward:", reward)
